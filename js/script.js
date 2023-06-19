701174bae59451cdbdd6977fce5da77e6ac4dac7
EnricoValente <enricovalente42@gmail.com>
2023-06-19 22:01:36 +0200

    completata milestone 2

--- js/script.js
+++ js/script.js
@@ -3,54 +3,38 @@
 // Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
 // Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 
-const firstImg = document.createElement('img');
-firstImg.setAttribute('src', 'img/01.webp');
 
-const secondImg = document.createElement('img');
-firstImg.setAttribute('src', 'img/02.webp');
-
-const thirdImg = document.createElement('img');
-firstImg.setAttribute('src', 'img/03.webp');
-
-const fourthImg = document.createElement('img');
-firstImg.setAttribute('src', 'img/04.webp');
-
-const fifthImg = document.createElement('img');
-firstImg.setAttribute('src', 'img/05.webp');
+const imagesList = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
 
 
-const imgList = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'];
-console.log('imgList', imgList, typeof imgList)
+const imagesContainer = document.querySelector('.images-container');
 
+for(let i = 0; i < imagesList.length; i++) {
+    console.log(i, imagesList[i]);
 
-
-let i = 1;
-
-for(i = 1; i < imgList.length; i++) {
-
-    imgList[i] = 'img/01.webp' + i
-
-    if (i == 1) {
-        document.querySelector('.container').innerHTML += `<img src="img/0${i}.webp"  class = "active">`
-    }
+    if (i == 0) {
+        imagesContainer.innerHTML = imagesContainer.innerHTML + `
     
-}
-
-const firstIcon = document.querySelector('.first-icon');
-const secondIcon = document.querySelector('.second-icon');
-
-let count = 1
-
-firstIcon.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    if(count < imgList.lenght) {
-        count++
-    }
-    else {
-        count = 0 
+        <div class="image-item">
+            <img src="${imagesList[i]}" alt="">
+        </div>
+    
+        `;
         
     }
-})
+    else {
+        imagesContainer.innerHTML = imagesContainer.innerHTML + `
+    
+        <div class="image-item none">
+            <img src="${imagesList[i]}" alt="">
+        </div>
+    
+        `;
+    }
+
+
+};
+
+
 
 
